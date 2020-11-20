47bd0c78340293e46d4abcb1af2e4552ff44fca6
Malathi P J <44761851+malu1112@users.noreply.github.com>
2020-11-19 18:13:59 -0700

    Update datematcher.py

--- datematcher.py
+++ datematcher.py
@@ -20,7 +20,7 @@ def date_matcher(input_date):
 
 
 if __name__ == '__main__':
-    sample_date = ['2020/11/19', '10/02/2020', '2020-12-01', '1-01-2020']
+    sample_date = ['2020/11/19', '10/02/2020', '2020-12-01', '01-01-2020']
     for date in sample_date:
         status, date_obj = date_matcher(date)
         print(f'Input Date: [{date}] [{status}] to [{date_obj}]')
