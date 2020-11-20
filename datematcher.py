099eaf7b0f3ba3c801d10411ef2e47d95f683a1a
Malathi P J <44761851+malu1112@users.noreply.github.com>
2020-11-19 18:18:31 -0700

    Update datematcher.py

--- datematcher.py
+++ datematcher.py
@@ -2,10 +2,10 @@ from re import match
 from datetime import datetime
 
 patterns = {
-    '%Y/%m/%d': r'^\s*\d{4}\/\d{2}\/\d{2}\s*$',
-    '%d/%m/%Y': r'^\s*\d{2}\/\d{2}\/\d{4}\s*$',
-    '%Y-%m-%d': r'^\s*\d{4}-\d{2}-\d{2}\s*$',
-    '%d-%m-%Y': r'^\s*\d{2}-\d{2}-\d{4}\s*$',
+    '%Y/%m/%d': r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$',
+    '%d/%m/%Y': r'^\s*\d{1,2}\/\d{1,2}\/\d{4}\s*$',
+    '%Y-%m-%d': r'^\s*\d{4}-\d{1,2}-\d{1,2}\s*$',
+    '%d-%m-%Y': r'^\s*\d{1,2}-\d{1,2}-\d{4}\s*$',
 }
 
 
@@ -15,12 +15,12 @@ def date_matcher(input_date):
             if match(pattern, input_date):
                 return 'CONVERTED', datetime.strptime(input_date, expected_format)
     except Exception as e:
-        return 'EXCEPTION', datetime.now()
-    return 'NOT CONVERTED', datetime.now()
+        return 'EXCEPTION', datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    return 'NOT CONVERTED', datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
 
 
 if __name__ == '__main__':
-    sample_date = ['2020/11/19', '10/02/2020', '2020-12-01', '01-01-2020']
+    sample_date = ['2020/11/19', '10/2/2020', '2020-12-01', '1-01-2020']
     for date in sample_date:
         status, date_obj = date_matcher(date)
         print(f'Input Date: [{date}] [{status}] to [{date_obj}]')
